2055c236a0f159f5c4ed8d24a6ee9af56dcaf0c6
Miraliba Jadeja <134034557+Miraliba11Jadeja@users.noreply.github.com>
2024-09-11 19:13:19 +0530

    Login Screen Updated

---
+++ lib/screen/Admin/LoginScreen.dart
@@ -0,0 +1,194 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  runApp(SchoolBridgeApp());
+}
+
+class SchoolBridgeApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  bool _obscureText = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          // Triangle with logo
+          Stack(
+            alignment: Alignment.center,
+            children: [
+              ClipPath(
+                clipper: InvertedTriangleClipper(),
+                child: Container(
+                  height: 200,
+                  color: Color(0xFF134B70),
+                ),
+              ),
+              Positioned(
+                top: 90,
+                child: CircleAvatar(
+                  radius: 60.0,
+                  backgroundImage: AssetImage('assets/L.png'), // Replace with your image asset
+                  backgroundColor: Colors.white,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height : 80),
+          Expanded(
+            child: SingleChildScrollView(
+              padding: EdgeInsets.all(20.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // Username Field with Underline
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 25.0),
+                    child: TextField(
+                      controller: _usernameController,
+                      decoration: InputDecoration(
+                        labelText: 'Username',
+                        hintText: 'Enter your username',
+                        prefixIcon: Icon(Icons.person),
+                        border: InputBorder.none, // No border, only underline
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.grey),
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Color(0xFF134B70)),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 15.0),
+
+                  // Password Field with Underline
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 25.0),
+                    child: TextField(
+                      controller: _passwordController,
+                      obscureText: _obscureText,
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        hintText: 'Enter your password',
+
+                        prefixIcon: Icon(Icons.lock),
+                        suffixIcon: IconButton(
+                          icon: Icon(
+                            _obscureText ? Icons.visibility_off : Icons.visibility,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              _obscureText = !_obscureText;
+                            });
+                          },
+                        ),
+                        border: InputBorder.none, // No border, only underline
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.grey),
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Color(0xFF134B70)),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20.0),
+
+                  // Login Button
+                  Container(
+                    width: double.infinity,
+                    padding: EdgeInsets.symmetric(horizontal: 25.0),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        padding: EdgeInsets.symmetric(vertical: 15.0),
+                        backgroundColor: Color(0xFF134B70),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                      ),
+                      onPressed: () {
+                        // Perform login validation here
+                        // For now, let's navigate to the dashboard screen
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => DashboardScreen()),
+                        );
+                      },
+                      child: Text(
+                        'Login',
+                        style: TextStyle(fontSize: 18.0),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 15.0),
+
+                  // Forgot Password Link
+                  TextButton(
+                    onPressed: () {
+                      // Handle forgot password action here
+                    },
+                    child: Text(
+                      'Forgot Password?',
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class InvertedTriangleClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    path.lineTo(size.width, 0.0);
+    path.lineTo(size.width / 2, size.height);
+    path.lineTo(0.0, 0.0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
+}
+
+class DashboardScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Dashboard'),
+      ),
+      body: Center(
+        child: Text(
+          'Welcome to the Dashboard!',
+          style: TextStyle(fontSize: 24.0),
+        ),
+      ),
+    );
+  }
+}
--- lib/screen/LoginAs.dart
+++ lib/screen/LoginAs.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'LoginScreen.dart';
+import 'Admin/LoginScreen.dart';
 
 
 class LoginAs extends StatefulWidget {
@@ -17,93 +17,91 @@ class _DashboardState extends State<LoginAs> {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              // Top Container with Inverted Triangle and Circle Avatar
-              Stack(
-                alignment: Alignment.center,
-                children: [
-                  ClipPath(
-                    clipper: InvertedTriangleClipper(),
-                    child: Container(
-                      height: 208,
-                      color: Color(0xFF134B70), // Corrected color syntax
-                    ),
+        child: Column(
+          children: [
+            // Top Container with Inverted Triangle and Circle Avatar
+            Stack(
+              alignment: Alignment.center,
+              children: [
+                ClipPath(
+                  clipper: InvertedTriangleClipper(),
+                  child: Container(
+                    height: 208,
+                    color: Color(0xFF134B70), // Corrected color syntax
                   ),
-                  Positioned(
-                    top: 100, // Position the circle inside the triangle
-                    child: CircleAvatar(
-                      radius: 60,
-                      backgroundImage: AssetImage('assets/L.png'), // Adjust image path
-                      backgroundColor: Colors.white,
-                    ),
+                ),
+                Positioned(
+                  top: 100, // Position the circle inside the triangle
+                  child: CircleAvatar(
+                    radius: 60,
+                    backgroundImage: AssetImage('assets/L.png'), // Adjust image path
+                    backgroundColor: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+            
+            // Separate Container for "WHO ARE YOU?" Text
+            Container(
+              margin: EdgeInsets.only(top: 20),
+              padding: EdgeInsets.all(7),
+              decoration: BoxDecoration(
+                color: Color(0xFF134B70), // Same blue color as the top container
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: Text(
+                'WHO ARE YOU?',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+
+            SizedBox(height: 60),
+
+            // Options: Student, Teacher, Admin
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      RoleCard(
+                        imagePath: 'assets/Student1.png', // Adjust image path
+                        label: 'STUDENT',
+                        onTap: () {
+                          // Handle student role selection
+                        },
+                      ),
+                      RoleCard(
+                        imagePath: 'assets/Teacher1.png', // Adjust image path
+                        label: 'TEACHER',
+                        onTap: () {
+                          // Handle teacher role selection
+                        },
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20.0),
+                  RoleCard(
+                    imagePath: 'assets/ADMIN1.png', // Adjust image path
+                    label: 'ADMIN',
+                    onTap: () {
+                      // Handle admin role selection
+                      Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => LoginPage()),
+                      );
+                    },
                   ),
                 ],
               ),
-              
-              // Separate Container for "WHO ARE YOU?" Text
-              Container(
-                margin: EdgeInsets.only(top: 20),
-                padding: EdgeInsets.all(7),
-                decoration: BoxDecoration(
-                  color: Color(0xFF134B70), // Same blue color as the top container
-                  borderRadius: BorderRadius.circular(15),
-                ),
-                child: Text(
-                  'WHO ARE YOU?',
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-
-              SizedBox(height: 60),
-
-              // Options: Student, Teacher, Admin
-              Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        RoleCard(
-                          imagePath: 'assets/Student1.png', // Adjust image path
-                          label: 'STUDENT',
-                          onTap: () {
-                            // Handle student role selection
-                          },
-                        ),
-                        RoleCard(
-                          imagePath: 'assets/Teacher1.png', // Adjust image path
-                          label: 'TEACHER',
-                          onTap: () {
-                            // Handle teacher role selection
-                          },
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 20.0),
-                    RoleCard(
-                      imagePath: 'assets/ADMIN1.png', // Adjust image path
-                      label: 'ADMIN',
-                      onTap: () {
-                        // Handle admin role selection
-                        Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => LoginPage()),
-                        );
-                      },
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
@@ -115,9 +113,9 @@ class InvertedTriangleClipper extends CustomClipper<Path> {
   @override
   Path getClip(Size size) {
     Path path = Path();
-    path.moveTo(0, 0);
     path.lineTo(size.width, 0);
     path.lineTo(size.width / 2, size.height);
+    path.lineTo(0.0, 0.0);
     path.close();
     return path;
   }
