078da772c921567b16b15de7adc95ecc0bf0ad3b
Miraliba Jadeja <134034557+Miraliba11Jadeja@users.noreply.github.com>
2024-09-30 12:22:32 +0530

    Edit and add teacher completed

---
+++ lib/screen/Admin/EditTeacher.dart
@@ -0,0 +1,216 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io'; // For handling file operations
+
+class EditTeacher extends StatefulWidget {
+  @override
+  _EditTeacherState createState() => _EditTeacherState();
+}
+
+class _EditTeacherState extends State<EditTeacher> {
+  File? _profileImage; // To store the selected image
+
+  // Function to pick an image
+  Future<void> _pickImage() async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      setState(() {
+        _profileImage = File(pickedFile.path);
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xFF134B70),
+        title: Text('TEACHER PROFILE'),
+        leading: GestureDetector(
+          onTap: () {
+            Navigator.of(context).pop(); // Go back when back icon is pressed
+          },
+          child: Icon(Icons.arrow_back),
+        ),
+        actions: [
+          Builder(
+            builder: (context) {
+              return IconButton(
+                icon: Icon(Icons.menu),
+                onPressed: () {
+                  Scaffold.of(context).openEndDrawer(); // Open endDrawer on menu icon click
+                },
+              );
+            },
+          ),
+        ],
+      ),
+      endDrawer: Container(
+        width: MediaQuery.of(context).size.width * 0.6,
+        child: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              Container(
+                height: 100,
+                child: DrawerHeader(
+                  decoration: BoxDecoration(
+                    color: Color(0xFF134B70),
+                  ),
+                  child: Center(
+                    child: Text(
+                      '',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 24,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              buildDrawerItem(context, 'assets/teacher.png', 'Teachers'),
+              buildDrawerItem(context, 'assets/add.png', 'Subject'),
+              buildDrawerItem(context, 'assets/timetable.png', 'Schedule'),
+              buildDrawerItem(context, 'assets/chat.png', 'Feedback'),
+              buildDrawerItem(context, 'assets/calendar.png', 'Event'),
+              buildDrawerItem(context, 'assets/loudspeaker.png', 'Announcement'),
+              buildDrawerItem(context, 'assets/holidays.png', 'Holiday'),
+            ],
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              CircleAvatar(
+                radius: 50,
+                backgroundColor: Colors.grey[300],
+                backgroundImage: _profileImage != null ? FileImage(_profileImage!) : null,
+                child: _profileImage == null ? Icon(Icons.person, size: 50) : null,
+              ),
+              SizedBox(height: 10),
+              ElevatedButton.icon(
+                onPressed: _pickImage, // Call the image picker function
+                icon: Icon(Icons.edit, color: Colors.white),
+                label: Text(
+                  'CHANGE PROFILE PICTURE',
+                  style: TextStyle(color: Colors.white),
+                ),
+                style: ElevatedButton.styleFrom(
+                  primary: Color(0xFF134B70),
+                  elevation: 0,
+                ),
+              ),
+              SizedBox(height: 20),
+              buildSectionTitle('PERSONAL DETAILS'),
+              buildDetailRow('Name', 'ABCD', 'Contact No', '1234567890'),
+              buildDetailRow('Email', 'abc@def.com', 'Gender', 'Male/Female'),
+              buildDetailRow('Date Of Birth', 'DD/MM/YYYY', 'Aadhar No', 'ABCDABCD3828'),
+              SizedBox(height: 20),
+              buildSectionTitle('CONTACT DETAILS'),
+              buildDetailRow('Permanent Address', 'ABCD, House Name, Landmark', 'Present Address', 'ABCD, House Name, Landmark'),
+              buildDetailRow('City', 'Rajkot', 'PinCode', '360020'),
+              buildDetailRow('State', 'Gujarat', 'Country', 'India'),
+              SizedBox(height: 20),
+              buildSectionTitle('ACADEMIC DETAILS'),
+              buildDetailRow('Class', '2, 3, 4', 'Subject', 'Hindi'),
+              buildDetailRow('Education', 'BTech', 'Specification', 'Hindi'),
+              SizedBox(height: 20),
+              buildSectionTitle('ACCOUNT DETAILS'),
+              buildDetailRow('Username', 'abc11', 'Password', 'abc@1234'),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text('Saved successfully!'),
+                      duration: Duration(seconds: 2),
+                    ),
+                  );
+                },
+                child: Text('SUBMIT'),
+                style: ElevatedButton.styleFrom(
+                  primary: Color(0xFF134B70),
+                  padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                  textStyle: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  ListTile buildDrawerItem(BuildContext context, String asset, String title) {
+    return ListTile(
+      leading: Image.asset(asset, width: 30, height: 30),
+      title: Text(title, style: TextStyle(fontWeight: FontWeight.bold)),
+      onTap: () {
+        Navigator.of(context).pop();
+      },
+    );
+  }
+
+  Widget buildSectionTitle(String title) {
+    return Container(
+      width: double.infinity,
+      padding: EdgeInsets.all(8.0),
+      color: Color(0xFF134B70),
+      child: Text(
+        title,
+        style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+          fontSize: 18,
+        ),
+      ),
+    );
+  }
+
+  Widget buildDetailRow(String leftLabel, String leftValue, String rightLabel, String rightValue) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(leftLabel, style: TextStyle(fontWeight: FontWeight.bold)),
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: leftValue,
+                    contentPadding: EdgeInsets.symmetric(vertical: 10.0),
+                    border: UnderlineInputBorder(),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(width: 10),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(rightLabel, style: TextStyle(fontWeight: FontWeight.bold)),
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: rightValue,
+                    contentPadding: EdgeInsets.symmetric(vertical: 10.0),
+                    border: UnderlineInputBorder(),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/screen/Admin/TeacherListScreen.dart
+++ lib/screen/Admin/TeacherListScreen.dart
@@ -1,11 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:school_bridge_app/screen/Admin/AddTeacher.dart';
+import 'package:school_bridge_app/screen/Admin/EditTeacher.dart';
 
-class TeacherListScreen extends StatelessWidget {
-  final List<Map<String, String>> teachers = [
+class TeacherListScreen extends StatefulWidget {
+  @override
+  _TeacherListScreenState createState() => _TeacherListScreenState();
+}
+
+class _TeacherListScreenState extends State<TeacherListScreen> {
+  List<Map<String, String>> teachers = [
     {"name": "ABCD", "subject": "Hindi", "classes": "2, 3, 4"},
     {"name": "EFGH", "subject": "Math", "classes": "5, 6, 7"},
     {"name": "IJKL", "subject": "English", "classes": "8, 9, 10"},
   ];
+
+  // Function to delete a teacher after confirmation
+  void _deleteTeacher(int index) {
+    setState(() {
+      teachers.removeAt(index); // Remove the teacher at the given index
+    });
+  }
+
+  // Function to show confirmation dialog
+  void _showDeleteConfirmationDialog(BuildContext context, int index) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Delete Teacher'),
+          content: Text('Are you sure you want to delete this teacher?'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // Close the dialog
+              },
+              child: Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () {
+                _deleteTeacher(index); // Call delete function
+                Navigator.of(context).pop(); // Close the dialog
+              },
+              child: Text('Delete', style: TextStyle(color: Colors.red)),
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -38,80 +80,83 @@ class TeacherListScreen extends StatelessWidget {
           ),
         ],
       ),
-      endDrawer: Drawer(
-  child: ListView(
-    padding: EdgeInsets.zero,
-    children: <Widget>[
-      Container(
-        height: 100, // Adjust the height as needed
-        child: DrawerHeader(
-          decoration: BoxDecoration(
-            color: Color(0xFF134B70),
-          ),
-          child: Center(
-            child: Text(
-              '',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 24,
+      endDrawer: Container(
+        width: MediaQuery.of(context).size.width * 0.6, // Set drawer width to 60% of the screen width
+        child: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              Container(
+                height: 100,
+                width: 40, // Adjust the height as needed
+                child: DrawerHeader(
+                  decoration: BoxDecoration(
+                    color: Color(0xFF134B70),
+                  ),
+                  child: Center(
+                    child: Text(
+                      '',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 24,
+                      ),
+                    ),
+                  ),
+                ),
               ),
-            ),
+              ListTile(
+                leading: Image.asset('assets/teacher.png', width: 30, height: 30),
+                title: Text('Teachers', style: TextStyle(fontWeight: FontWeight.bold)),
+                onTap: () {
+                  Navigator.of(context).pop(); // Close drawer on tap
+                },
+              ),
+              ListTile(
+                leading: Image.asset('assets/add.png', width: 30, height: 30),
+                title: Text('Subject', style: TextStyle(fontWeight: FontWeight.bold)),
+                onTap: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+              ListTile(
+                leading: Image.asset('assets/timetable.png', width: 30, height: 30),
+                title: Text('Schedule', style: TextStyle(fontWeight: FontWeight.bold)),
+                onTap: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+              ListTile(
+                leading: Image.asset('assets/chat.png', width: 30, height: 30),
+                title: Text('Feedback', style: TextStyle(fontWeight: FontWeight.bold)),
+                onTap: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+              ListTile(
+                leading: Image.asset('assets/calendar.png', width: 30, height: 30),
+                title: Text('Event', style: TextStyle(fontWeight: FontWeight.bold)),
+                onTap: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+              ListTile(
+                leading: Image.asset('assets/loudspeaker.png', width: 30, height: 30),
+                title: Text('Announcement', style: TextStyle(fontWeight: FontWeight.bold)),
+                onTap: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+              ListTile(
+                leading: Image.asset('assets/holidays.png', width: 30, height: 30),
+                title: Text('Holiday', style: TextStyle(fontWeight: FontWeight.bold)),
+                onTap: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
           ),
         ),
       ),
-      ListTile(
-        leading: Image.asset('assets/teacher.png', width: 30, height: 30),
-        title: Text('Teachers', style: TextStyle(fontWeight: FontWeight.bold)),
-        onTap: () {
-          Navigator.of(context).pop(); // Close drawer on tap
-        },
-      ),
-      ListTile(
-        leading: Image.asset('assets/add.png', width: 30, height: 30),
-        title: Text('Subject', style: TextStyle(fontWeight: FontWeight.bold)),
-        onTap: () {
-          Navigator.of(context).pop();
-        },
-      ),
-      ListTile(
-        leading: Image.asset('assets/timetable.png', width: 30, height: 30),
-        title: Text('Schedule', style: TextStyle(fontWeight: FontWeight.bold)),
-        onTap: () {
-          Navigator.of(context).pop();
-        },
-      ),
-      ListTile(
-        leading: Image.asset('assets/chat.png', width: 30, height: 30),
-        title: Text('Feedback', style: TextStyle(fontWeight: FontWeight.bold)),
-        onTap: () {
-          Navigator.of(context).pop();
-        },
-      ),
-      ListTile(
-        leading: Image.asset('assets/calendar.png', width: 30, height: 30),
-        title: Text('Event', style: TextStyle(fontWeight: FontWeight.bold)),
-        onTap: () {
-          Navigator.of(context).pop();
-        },
-      ),
-      ListTile(
-        leading: Image.asset('assets/loudspeaker.png', width: 30, height: 30),
-        title: Text('Announcement', style: TextStyle(fontWeight: FontWeight.bold)),
-        onTap: () {
-          Navigator.of(context).pop();
-        },
-      ),
-      ListTile(
-        leading: Image.asset('assets/holidays.png', width: 30, height: 30),
-        title: Text('Holiday', style: TextStyle(fontWeight: FontWeight.bold)),
-        onTap: () {
-          Navigator.of(context).pop();
-        },
-      ),
-    ],
-  ),
-),
-
       body: ListView.builder(
         padding: EdgeInsets.all(16.0),
         itemCount: teachers.length,
@@ -165,13 +210,18 @@ class TeacherListScreen extends StatelessWidget {
                         IconButton(
                           icon: Icon(Icons.edit, color: Colors.blue),
                           onPressed: () {
-                            // Handle edit action
+                            // Navigate to TeacherProfileScreen
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => EditTeacher()),
+                            );
                           },
                         ),
                         IconButton(
                           icon: Icon(Icons.delete, color: Colors.red),
                           onPressed: () {
-                            // Handle delete action
+                            // Show confirmation dialog before deleting
+                            _showDeleteConfirmationDialog(context, index);
                           },
                         ),
                       ],
@@ -186,6 +236,10 @@ class TeacherListScreen extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           // Handle add teacher action
+          Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => AddTeacher()),
+          );
         },
         child: Icon(Icons.add),
         backgroundColor: Color(0xFF134B70),
