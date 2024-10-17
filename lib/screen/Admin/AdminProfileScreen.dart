56cc10778e067f305e18798318f7dec127233ca6
Miraliba Jadeja <mjadeja482@rku.ac.in>
2024-10-17 11:09:17 +0530

    AdminProfileValuesFetched

--- lib/screen/Admin/AdminProfileScreen.dart
+++ lib/screen/Admin/AdminProfileScreen.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 
 class AdminProfileScreen extends StatefulWidget {
   @override
@@ -10,23 +12,9 @@ class AdminProfileScreen extends StatefulWidget {
 class _AdminProfileScreenState extends State<AdminProfileScreen> {
   bool isEditing = false;
 
-  // Initial profile data
-  String name = 'ABCD';
-  String email = 'abc@def.com';
-  String dob = 'DD/MM/YYYY';
-  String contactNo = '1234567890';
-  String gender = 'Male/Female';
-  String aadharNo = 'ABCDBD123456';
-
-  String permanentAddress = 'ABCD, House Name, Landmark';
-  String city = 'Rajkot';
-  String state = 'Gujarat';
-  String pinCode = '360020';
-  String presentAddress = 'ABCD, House Name, Landmark';
-  String country = 'India';
-
   // Image-related variables
   File? _profileImage;
+  String? _profileImageUrl;
   final ImagePicker _picker = ImagePicker();
 
   // Controllers to manage text field inputs
@@ -48,20 +36,20 @@ class _AdminProfileScreenState extends State<AdminProfileScreen> {
   void initState() {
     super.initState();
 
-    // Initialize controllers with current data
-    nameController = TextEditingController(text: name);
-    emailController = TextEditingController(text: email);
-    dobController = TextEditingController(text: dob);
-    contactController = TextEditingController(text: contactNo);
-    genderController = TextEditingController(text: gender);
-    aadharController = TextEditingController(text: aadharNo);
+    // Initialize controllers with empty values
+    nameController = TextEditingController();
+    emailController = TextEditingController();
+    dobController = TextEditingController();
+    contactController = TextEditingController();
+    genderController = TextEditingController();
+    aadharController = TextEditingController();
 
-    permanentAddressController = TextEditingController(text: permanentAddress);
-    cityController = TextEditingController(text: city);
-    stateController = TextEditingController(text: state);
-    pinCodeController = TextEditingController(text: pinCode);
-    presentAddressController = TextEditingController(text: presentAddress);
-    countryController = TextEditingController(text: country);
+    permanentAddressController = TextEditingController();
+    cityController = TextEditingController();
+    stateController = TextEditingController();
+    pinCodeController = TextEditingController();
+    presentAddressController = TextEditingController();
+    countryController = TextEditingController();
   }
 
   @override
@@ -110,93 +98,119 @@ class _AdminProfileScreenState extends State<AdminProfileScreen> {
             ),
           ),
         ),
-
         title: Text('Admin Profile'),
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            children: [
-              // Profile Picture Section
-              CircleAvatar(
-                radius: 50,
-                backgroundColor: Colors.grey[300],
-                backgroundImage: _profileImage != null ? FileImage(_profileImage!) : null,
-                child: _profileImage == null
-                    ? Icon(Icons.person, size: 60, color: Colors.white)
-                    : null,
-              ),
-              SizedBox(height: 10),
-              ElevatedButton(
-                onPressed: () {
-                  _showImageSourceActionSheet(context);
-                },
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.white,
-                  side: BorderSide(color: Colors.black),
-                ),
-                child: Text(
-                  'Change Profile Picture',
-                  style: TextStyle(color: Colors.black),
-                ),
-              ),
-              SizedBox(height: 20),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('Admin').snapshots(),
+        builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (!snapshot.hasData) {
+            return Center(child: CircularProgressIndicator());
+          }
 
-              // Personal Details Section
-              buildSectionTitle('PERSONAL DETAILS'),
-              buildDetailsRow('Name', nameController, 'Email', emailController),
-              buildDetailsRow('Date Of Birth', dobController, 'Contact No', contactController),
-              buildDetailsRow('Gender', genderController, 'Aadhar No.', aadharController),
+          // Assuming you're working with a single admin document
+          var adminData = snapshot.data!.docs.first; // Get the first document in the 'admin' collection
 
-              SizedBox(height: 20),
+          // Update text field controllers with Firestore data
+          nameController.text = adminData['Name'] ?? '';   // Set the fetched name
+          emailController.text = adminData['Email'] ?? ''; // Set the fetched email
+          dobController.text = adminData['DOB'] ?? '';     // Set the fetched DOB
+          contactController.text = adminData['Contact'] ?? '';
+          genderController.text = adminData['Gender'] ?? '';
+          aadharController.text = adminData['Aadhar'] ?? '';
+          permanentAddressController.text = adminData['PermanentA'] ?? '';
+          cityController.text = adminData['City'] ?? '';
+          stateController.text = adminData['State'] ?? '';
+          pinCodeController.text = adminData['Pincode'] ?? '';
+          presentAddressController.text = adminData['PresentA'] ?? '';
+          countryController.text = adminData['Country'] ?? '';
 
-              // Contact Details Section
-              buildSectionTitle('CONTACT DETAILS'),
-              buildDetailsRow('Permanent Address', permanentAddressController, 'City', cityController),
-              buildDetailsRow('State', stateController, 'PinCode', pinCodeController),
-              buildDetailsRow('Present Address', presentAddressController, 'Country', countryController),
-
-              SizedBox(height: 40),
-
-              // Edit/Save Button
-              ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    if (isEditing) {
-                      // Save the updated data
-                      name = nameController.text;
-                      email = emailController.text;
-                      dob = dobController.text;
-                      contactNo = contactController.text;
-                      gender = genderController.text;
-                      aadharNo = aadharController.text;
-
-                      permanentAddress = permanentAddressController.text;
-                      city = cityController.text;
-                      state = stateController.text;
-                      pinCode = pinCodeController.text;
-                      presentAddress = presentAddressController.text;
-                      country = countryController.text;
-                    }
-                    isEditing = !isEditing; // Toggle the editing state
-                  });
-                },
-                style: ElevatedButton.styleFrom(
-                  primary: Color(0xFF134B70), // Background color of the button
-                  padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
+          return SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                children: [
+                  // Profile Picture Section
+                  CircleAvatar(
+                    radius: 50,
+                    backgroundColor: Colors.grey[300],
+                    backgroundImage: _profileImage != null ? FileImage(_profileImage!) : null,
+                    child: _profileImage == null
+                        ? Icon(Icons.person, size: 60, color: Colors.white)
+                        : null,
                   ),
-                ),
-                child: Text(
-                  isEditing ? 'Save' : 'Edit Profile',
-                  style: TextStyle(fontSize: 18, color: Colors.white),
-                ),
+                  SizedBox(height: 10),
+                  ElevatedButton(
+                    onPressed: () {
+                      _showImageSourceActionSheet(context);
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.white,
+                      side: BorderSide(color: Colors.black),
+                    ),
+                    child: Text(
+                      'Change Profile Picture',
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+
+                  // Personal Details Section
+                  buildSectionTitle('PERSONAL DETAILS'),
+                  buildDetailsRow('Name', nameController, 'Email', emailController),
+                  buildDetailsRow('Date Of Birth', dobController, 'Contact No', contactController),
+                  buildDetailsRow('Gender', genderController, 'Aadhar No.', aadharController),
+
+                  SizedBox(height: 20),
+
+                  // Contact Details Section
+                  buildSectionTitle('CONTACT DETAILS'),
+                  buildDetailsRow('Permanent Address', permanentAddressController, 'City', cityController),
+                  buildDetailsRow('State', stateController, 'PinCode', pinCodeController),
+                  buildDetailsRow('Present Address', presentAddressController, 'Country', countryController),
+
+                  SizedBox(height: 40),
+
+                  // Edit/Save Button
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        if (isEditing) {
+                          // Save the updated data
+                          FirebaseFirestore.instance.collection('admin').doc(adminData.id).update({
+                            'name': nameController.text,
+                            'email': emailController.text,
+                            'dob': dobController.text,
+                            'contactNo': contactController.text,
+                            'gender': genderController.text,
+                            'aadharNo': aadharController.text,
+                            'permanentAddress': permanentAddressController.text,
+                            'city': cityController.text,
+                            'state': stateController.text,
+                            'pinCode': pinCodeController.text,
+                            'presentAddress': presentAddressController.text,
+                            'country': countryController.text,
+                          });
+                        }
+                        isEditing = !isEditing; // Toggle the editing state
+                      });
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xFF134B70), // Background color of the button
+                      padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    child: Text(
+                      isEditing ? 'Save' : 'Edit Profile',
+                      style: TextStyle(fontSize: 18, color: Colors.white),
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
-        ),
+            ),
+          );
+        }
       ),
     );
   }
